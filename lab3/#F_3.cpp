ff2fb89e44e8caef54c053e2d44503bbeda2ea4c
TanyaTomashuk <71095340+TanyaTomashuk@users.noreply.github.com>
2021-03-15 12:37:30 +0300

    Update #F_3.cpp

--- lab3/#F_3.cpp
+++ lab3/#F_3.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
 int* my_slightly_dumb_reallocation(int* source, unsigned int n_old, unsigned int n_new){
     if(n_new == 0){
         delete [] source;
@@ -11,4 +16,23 @@ int* my_slightly_dumb_reallocation(int* source, unsigned int n_old, unsigned int
         delete [] source;
     }
     return q;
+}
+
+int main() {
+    unsigned int n, i;
+    cin >> n;
+    int *a = my_slightly_dumb_reallocation(NULL, 0, n / 2);
+    for (i = 0; i < n / 2; i++)
+        cin >> a[i];
+    a = my_slightly_dumb_reallocation(a, n / 2, n);
+    for (; i < n; i++)
+        cin >> a[i];
+    int sum = 0;
+    for (i = 0; i < n; i++)
+        sum += a[i];
+    cout << sum << endl;
+    a = my_slightly_dumb_reallocation(a, n, n / 2);
+    a = my_slightly_dumb_reallocation(a, n / 2, 0);
+    a = my_slightly_dumb_reallocation(a, 0, 0);
+    return 0;
 }
